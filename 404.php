e35cb778a4d5e44700ccecf3e1083d4b363b4365
xAballon <ebner.fabian1@hakspittal.at>
2025-04-29 11:18:07 +0200

    404 Seite hinzugefügt mit Style

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+    <title>404 - Error | MyFinance</title>
+</head>
+
+<body>
+
+    <header>
+        <?php include 'header.php'; ?>
+    </header>
+
+    <div class="error-card">
+        <h1>404</h1>
+        <p>Entschuldigung! Diese Seite gibt es leider nicht :(</p>
+        <a href="/">Zurück zur Startseite</a>
+    </div>
+
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
+
+</body>
+
+</html>
